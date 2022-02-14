2cc98b5b04472901c307a270f60f7379a8d8f621
pokori20 <93557699+pokori20@users.noreply.github.com>
2022-02-14 12:55:16 +0000

    :memo: 休み希望登録ページの作成
    休み希望申請ボタンの表示

--- app/controllers/public/submit_offs_controller.rb
+++ app/controllers/public/submit_offs_controller.rb
@@ -1,19 +1,25 @@
 class Public::SubmitOffsController < ApplicationController
   before_action :authenticate_public!
-  
+
   def new
     @off = Off.new
   end
-  
+
   def create
     @off = Off.new(off_params)
+    @off.shop_id = current_public.shop_id
+    @off.public_id = current_public.id
     @off.save
+    # submit_offの保存を定義
+    # @submit_off = SubmitOff.new(submit_off_params)
+    # @submit_off.shop_id = current_public.shop_id
+    # @submit_off.save
     redirect_back(fallback_location: root_path)
   end
-  
+
   private
   def off_params
     params.require(:off).permit(:date)
   end
-  
+
 end
