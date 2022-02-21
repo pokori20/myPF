835482e9c9cf2a647b844ba46b60bff4962dce57
pokori20 <93557699+pokori20@users.noreply.github.com>
2022-02-21 15:36:23 +0000

    :lipstick: Adjustment layout
    シフト投稿ページのレイアウト修正、offs controllerの作成とsubmit_off controllerの削除

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
    resources :publics, only: [:index, :create, :new, :edit, :update, :show]
   resources :shops, only: [:index, :create, :edit, :update]
   resources :shifts, only: [:new, :index, :create, :show]
-  resources :submit_offs, only: [:update, :show]
+  resources :offs, only: [:show]
   end
 
     # 従業員用
@@ -25,8 +25,8 @@ Rails.application.routes.draw do
     patch 'publics/update' => 'publics#update'
     #shiftsコントローラー
     resources :shifts, only: [:show]
-    resources :submit_offs, only: [:new, :create, :comment]
-    resource :submit_offs, only: [:destroy], as: 'delete_submit_off'
+    resources :offs, only: [:new, :create, :comment]
+    resource :offs, only: [:destroy], as: 'delete_off'
   end
 
 
---
+++ test/controllers/public/offs_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Public::OffsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
