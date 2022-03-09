6777f27b999e4c1ad427be53b974ff8aa0f30b8a
pokori20 <93557699+pokori20@users.noreply.github.com>
2022-03-09 11:48:27 +0000

    :construction: add cord admin/offs/show
    管理者側に休み希望一覧にシフトパターンを表示

--- app/controllers/admin/offs_controller.rb
+++ app/controllers/admin/offs_controller.rb
@@ -2,5 +2,6 @@ class Admin::OffsController < ApplicationController
   before_action :authenticate_admin!
   def show
     @publics = Public.where(shop_id: params[:id])
+    @patterns = Pattern.all
   end
 end
